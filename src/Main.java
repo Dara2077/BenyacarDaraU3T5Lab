48d3f58203ea5b5a160dcbeb8212baa36073c171
Dara Benyacar <dbenyacar1@gmail.com>
2022-11-02 17:42:39 -0400

    Finished lab

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-- Set B --");
+        // CODE SEGMENT D
+        int p = 5;
+        int q = 0;
 
-        boolean isRaining = false;
-        double bank = 10.50;
-        int minutes = 45;
-        boolean shop;
-        boolean stay;
-
-        shop = (bank >= 8.00) && (minutes < 60) && !isRaining;
-        System.out.println(shop);
-
-        stay = !(bank > 5) || minutes > 30 || isRaining;
-        System.out.println(stay);
-
-        int score = 65;
-        if (score < 0 || score > 100 || score % 2 != 0)
+        if (p > 0 && (p / q) == 2)
         {
-            System.out.println("Illegal score");
-        }
-        else if (score >= 0 && score <= 100 && score % 2 == 0)
-        {
-            System.out.println("Legal score");
+            System.out.println("it’s true :)");
         }
         else
         {
-            System.out.println("Invalid!");
+            System.out.println("it’s false :(");
         }
-
-        System.out.println("- part 2 -");
-
-        score = 80;
-        if (score < 0 || score > 100 || score % 2 != 0)
-        {
-            System.out.println("Illegal score");
-        }
-        else if (score >= 0 && score <= 100 && score % 2 == 0)
-        {
-            System.out.println("Legal score");
-        }
-        else
-        {
-            System.out.println("Invalid!");
-        }
-
-        String word = "Apple";
-        if (!word.toLowerCase().equals("apple"))
-        {
-            System.out.println("beep!");
-        }
-        else
-        {
-            System.out.println("boop!");
-        }
-
     }
 }
 
